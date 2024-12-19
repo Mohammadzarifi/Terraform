a0fa9da88bbf6f8be38d109e969a6e7017d6b5ac
Mohammadzarifi <iqbalzarifi@icloud.com>
2024-12-18 21:24:56 -0600

    this is the last push  of session3

---
+++ session-3/variable.tf
@@ -0,0 +1,11 @@
+variable "istance_type" {
+    type = "string"
+    default = "t2.micro"
+    description = "this is instance type"
+}
+
+variable "instance_name" {
+ type = "string"
+    default = "t2.micro"
+    description = "this is instance name" 
+}
